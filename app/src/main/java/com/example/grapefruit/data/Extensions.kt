88c2e23755e745f2e10ce0d3664790f6c3f0c81c
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2023-11-27 18:55:52 +0100

    handleWithFlow extension

---
+++ app/src/main/java/com/example/grapefruit/data/Extensions.kt
@@ -0,0 +1,21 @@
+package com.example.grapefruit.data
+
+import hu.blueberry.cloud.ResourceState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+fun <T> handleWithFlow (call: suspend () -> T) : Flow<ResourceState<T>> {
+    return flow{
+        emit(ResourceState.Loading())
+
+        val response = call()
+
+        emit(ResourceState.Success(response))
+
+    }.catch{
+            e -> emit(ResourceState.Error(e))
+    }.flowOn(Dispatchers.IO)
+}
--- app/src/main/java/com/example/grapefruit/data/repository/DriveRepository.kt
+++ app/src/main/java/com/example/grapefruit/data/repository/DriveRepository.kt
@@ -1,37 +1,40 @@
 package com.example.grapefruit.data.repository
 
+import com.example.grapefruit.data.handleWithFlow
 import hu.blueberry.cloud.google.DriveManager
 
 import hu.blueberry.cloud.ResourceState
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 
 class DriveRepository @Inject constructor(
     private var driveManager: DriveManager
 ) {
+    suspend fun createFolder(name: String): Flow<ResourceState<String>> {
+        return handleWithFlow { driveManager.createFolder(name) }
+    }
 
+    suspend fun createSpreadSheetInFolder(
+        folderId: String,
+        sheetName: String
+    ): Flow<ResourceState<String>> {
+        return handleWithFlow { driveManager.createSpreadSheetInFolder(folderId, sheetName) }
+    }
 
-    suspend fun createFolder(name:String) : Flow<ResourceState<String?>> {
-        return flow{
-            emit(ResourceState.Loading())
+    suspend fun createFile(
+        fileName: String,
+        parents: List<String>,
+        mimeType: String
+    ): Flow<ResourceState<String>> {
+        return handleWithFlow { driveManager.createFile(fileName, parents, mimeType) }
 
-            val response = driveManager.createFolder(name)
-
-            emit(ResourceState.Success(response))
-
-        }.catch{
-            e -> emit(ResourceState.Error(e))
-        }.flowOn(Dispatchers.IO)
     }
 
 
-
-
-
 }
+
+
+
+
 
