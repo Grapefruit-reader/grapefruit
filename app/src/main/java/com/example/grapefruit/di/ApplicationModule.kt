f9091a36ce813a5281d409657ff93e4fba789886
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2023-11-27 19:42:00 +0100

    MemoryDatabase

--- app/src/main/java/com/example/grapefruit/di/ApplicationModule.kt
+++ app/src/main/java/com/example/grapefruit/di/ApplicationModule.kt
@@ -1,11 +1,13 @@
 package com.example.grapefruit.di
 
 import android.content.Context
+import com.example.grapefruit.model.MemoryDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
@@ -14,5 +16,9 @@ class ApplicationModule {
 
     @Provides
     fun provideApplicationContext(@ApplicationContext context: Context)= context
+
+    @Provides
+    @Singleton
+    fun provideMemoryDatabase() = MemoryDatabase()
 
 }
