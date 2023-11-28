b75fd66a098764f503c0229655b48453d9542c6a
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2023-11-28 13:30:25 +0100

    Write Data to excel

--- app/src/main/java/com/example/grapefruit/data/viewmodel/WriteVoteViewModel.kt
+++ app/src/main/java/com/example/grapefruit/data/viewmodel/WriteVoteViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.grapefruit.data.repository.VoteRepository
 import com.example.grapefruit.model.MemoryDatabase
+import com.example.grapefruit.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.blueberry.cloud.ResourceState
 import hu.blueberry.cloud.google.GoogleSheetsManager
@@ -18,6 +19,20 @@ class WriteVoteViewModel @Inject constructor(
     private var voteRepository: VoteRepository,
     private var memoryDatabase: MemoryDatabase
 ): ViewModel(){
+
+    var user : MutableStateFlow<User?> = MutableStateFlow(null)
+
+    fun setData(user:User){
+        buttonState.value = ButtonStates.ACTIVE
+        this.user.value = user
+    }
+
+    var buttonState = MutableStateFlow(ButtonStates.DISABLED)
+
+    object ButtonStates {
+         val ACTIVE = "active"
+        val DISABLED = "disaled"
+    }
     object Ranges{
         const val YES_RANGE = "A:B"
         const val NO_RANGE = "D:E"
@@ -27,7 +42,20 @@ class WriteVoteViewModel @Inject constructor(
     private var voteWriting : MutableStateFlow<ResourceState<String>> = MutableStateFlow(ResourceState.Initial())
 
 
+    fun writeFromData(func : (user:User) -> Unit){
+        user.value?.let {
+           func(it)
+        }
+    }
+
+    fun writeYesFromData() = writeFromData { user -> writeYes(user.name,user.share ) }
+    fun writeNoFromData() = writeFromData { user -> writeNo(user.name,user.share ) }
+    fun writeResidesFromData() = writeFromData { user -> writeResides(user.name,user.share ) }
+
+
     fun writeYes(name:String, share:Double){
+        buttonState.value =  ButtonStates.DISABLED
+
         val values = mutableListOf(mutableListOf<Any>(name, share))
         val range = memoryDatabase.workSheet + Ranges.YES_RANGE
 
@@ -37,6 +65,8 @@ class WriteVoteViewModel @Inject constructor(
     }
 
     fun writeNo(name:String, share:Double){
+        buttonState.value =  ButtonStates.DISABLED
+
         val values = mutableListOf(mutableListOf<Any>(name, share))
         val range = memoryDatabase.workSheet + Ranges.NO_RANGE
 
@@ -46,6 +76,8 @@ class WriteVoteViewModel @Inject constructor(
     }
 
     fun writeResides(name:String, share:Double){
+        buttonState.value =  ButtonStates.DISABLED
+
         val values = mutableListOf(mutableListOf<Any>(name, share))
         val range = memoryDatabase.workSheet + Ranges.RESIDE_RANGE
 
