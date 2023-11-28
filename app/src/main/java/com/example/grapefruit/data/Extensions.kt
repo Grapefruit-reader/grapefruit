350e664012867388fd0fdc03f97cd8d2cb8522a2
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2023-11-28 14:44:12 +0100

    handleResponse

--- app/src/main/java/com/example/grapefruit/data/Extensions.kt
+++ app/src/main/java/com/example/grapefruit/data/Extensions.kt
@@ -1,5 +1,6 @@
 package com.example.grapefruit.data
 
+import android.util.Log
 import hu.blueberry.cloud.ResourceState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -7,15 +8,30 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-suspend fun <T> handleWithFlow (call: suspend () -> T) : Flow<ResourceState<T>> {
-    return flow{
+suspend fun <T> handleWithFlow(call: suspend () -> T): Flow<ResourceState<T>> {
+    return flow {
         emit(ResourceState.Loading())
 
         val response = call()
 
         emit(ResourceState.Success(response))
 
-    }.catch{
-            e -> emit(ResourceState.Error(e))
+    }.catch { e ->
+        emit(ResourceState.Error(e))
     }.flowOn(Dispatchers.IO)
+}
+
+fun <T> handleResponse(
+    resource:ResourceState<T>,
+    onInitial: () -> Unit = {},
+    onLoading: () -> Unit = {},
+    onSuccess: (data: T) -> Unit = {},
+    onError: (error: Any) -> Unit = {},
+) {
+    when(resource){
+        is ResourceState.Initial -> onInitial()
+        is ResourceState.Loading -> onLoading()
+        is  ResourceState.Success -> onSuccess(resource.data)
+        is ResourceState.Error -> onError(resource.error)
+    }
 }
